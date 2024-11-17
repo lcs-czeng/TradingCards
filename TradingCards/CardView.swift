79d814653998afeccf0c0a29ee15c259508b4710
Courage Zeng <czeng@lcs.on.ca>
2024-11-16 22:29:00 -0500

    added the american flag, cropped my background, removed color overlay, added and completed the title with custom fonts and american flag.

--- TradingCards/CardView.swift
+++ TradingCards/CardView.swift
@@ -10,12 +10,37 @@ import SwiftUI
 struct CardView: View {
     var body: some View {
         ZStack {
+            //Layer #1
             Image("graybackground")
                 .resizable()
                 .scaledToFill()
-                .ignoresSafeArea()
-            Color.black.opacity(0.1)
-                .ignoresSafeArea()
+                .frame(width: 400,height: 400)
+           
+            //Layer #2
+            VStack(spacing: -25) {
+                HStack {
+                    Text("HOUSE")
+                        .font(.custom("Bourgeois-BoldCond", size: 80))
+                        .foregroundColor(.white.opacity(0.8))
+                        .minimumScaleFactor(0.5)
+                        .kerning(8)
+                    Image("americanflag")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:80, height: 80)
+                }
+                .padding(.top, 100)
+                HStack(alignment: .firstTextBaseline) {
+                    Text("of")
+                        .font(.custom("Bourgeois-BookCondItal", size: 80))
+                        .foregroundColor(.white.opacity(0.8))
+                    Text("CARDS")
+                        .font(.custom("Bourgeois-BoldCond", size: 80))
+                        .foregroundColor(.white.opacity(0.8))
+                        .kerning(8)
+                }
+                Spacer()
+            }
         }
     }
 }
