76215841c2267d8b5ef2cd24522531504d6f6479
Courage Zeng <czeng@lcs.on.ca>
2024-11-29 23:25:01 -0500

    created a tab view for the cards.

---
+++ TradingCards/Views/CardsTabView.swift
@@ -0,0 +1,18 @@
+//
+//  CardsTabView.swift
+//  TradingCards
+//
+//  Created by 曾梓恒 on 2024/11/29.
+//
+
+import SwiftUI
+
+struct CardsTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardsTabView()
+}
